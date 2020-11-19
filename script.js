a57f206781b43fc95573d70db446beab1b2d533b
Grant <beauhengst@gmail.com>
2020-11-18 20:44:35 -0500

    added confirmation for each character to be included. Added an "if" statement to make sure that a character is chosen

--- script.js
+++ script.js
@@ -3,22 +3,42 @@ var generateBtn = document.querySelector("#generate");
 var lowerCase = ["a", "b", "c", "d", "e" , "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", 
 "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", 
-"W","X", "Y" "Z"];
+"W","X", "Y", "Z"];
+var specialCharacter = ["!", "@","#", "$", "%", "^", "*", "&", "?", "/", "+"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharacter = ["!", "@","#", "$", "%", "^", "*", "&", "?", "/", "+"]
+
 
 
 
 
 // Write password to the #password input
-function
+
 
 function writePassword() {
-  
+
+  var userLowerCase = confirm("Do you want to include lowercase letters in your password?");
+  var userUpperCase = confirm("Do you want to include uppercase letters in your password?");
+  var userSpecialCharacter = confirm("Do you want to include special characters?")
+  var userNumber = confirm("Do you want to include numbers?")
+
+  if (userLowerCase == false && userUpperCase == false && userSpecialCharacter == false && userNumber == false) {
+    alert("You have to pick one to generate a password");
+    userLowerCase = confirm("Do you want to include lowercase letters in your password?");
+    userUpperCase = confirm("Do you want to include uppercase letters in your password?");
+    userSpecialCharacter = confirm("Do you want to include special characters?")
+    userNumber = confirm("Do you want to include numbers?")
+
+  }
+
+  var passwordLength 
+
+
   var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+
 
 }
 
